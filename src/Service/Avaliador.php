d8316239f84a042d15126ee0ba1a38fe5b4862d7
IgorGuariroba <1g0r.guari@gmail.com>
2021-08-05 23:28:37 -0300

    Como criar testes completos que garante o bom funcionamento do codigo

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -2,6 +2,7 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
@@ -12,12 +13,20 @@ class Avaliador
     public function avalia(Leilao $leilao): void
     {
         foreach ($leilao->getLances() as $lance) {
-            if ( $lance->getValor() > $this->maiorValor) {
+            if ($lance->getValor() > $this->maiorValor) {
                 $this->maiorValor = $lance->getValor();
-            } 
-            if ($lance->getValor() < $this->menorValor) {
-                $this->menorValor = $lance->getValor(); 
             }
+
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
+            }
+
+            $lances = $leilao->getLances();
+            usort($lances, function (Lance $lance1, Lance $lance2) {
+                return $lance2->getValor() - $lance1->getValor();
+            });
+
+            $this->maioresLances = array_slice($lances, 0, 3);
         }
     }
 
@@ -29,5 +38,10 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -1,19 +1,16 @@
 <?php
-/**
- *  Comando utilizado para rodar o teste de validação 
- *  vendor/bin/phpunit --colors tests/
- */
-namespace Alura\Leilao\test\Service;
+
+namespace Alura\Leilao\Tests\Service;
 
 use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Model\Usuario;
-use PHPUnit\Framework\TestCase;
 use Alura\Leilao\Service\Avaliador;
+use PHPUnit\Framework\TestCase;
 
 class AvaliadorTest extends TestCase
 {
-    public function testEmOrdeCrecente()
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemCrescente()
     {
         // Arrange - Given
         $leilao = new Leilao('Fiat 147 0KM');
@@ -35,7 +32,7 @@ class AvaliadorTest extends TestCase
         self::assertEquals(2500, $maiorValor);
     }
 
-    public function testEmOrdeDecrecente()
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemDecrescente()
     {
         // Arrange - Given
         $leilao = new Leilao('Fiat 147 0KM');
@@ -57,8 +54,7 @@ class AvaliadorTest extends TestCase
         self::assertEquals(2500, $maiorValor);
     }
 
-
-    public function testRecuperarOMenorValorEmOrdeDecrecente()
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemDecrescente()
     {
         // Arrange - Given
         $leilao = new Leilao('Fiat 147 0KM');
@@ -80,7 +76,7 @@ class AvaliadorTest extends TestCase
         self::assertEquals(2000, $menorValor);
     }
 
-    public function testRecuperaMenorValorEmOrdeCrecente()
+    public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCrescente()
     {
         // Arrange - Given
         $leilao = new Leilao('Fiat 147 0KM');
@@ -102,4 +98,28 @@ class AvaliadorTest extends TestCase
         self::assertEquals(2000, $menorValor);
     }
 
+    public function testAvaliadorDeveRetornar3MaioresValores()
+    {
+
+        $leilao = new Leilao('Fiat 147 0KM');
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+        $ana = new Usuario('Ana');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($ana, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($jorge, 1700));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiores = $leiloeiro->getMaioresLances();
+
+        static::assertCount(3, $maiores);
+        static::assertEquals(2000, $maiores[0]->getValor());
+        static::assertEquals(1700, $maiores[1]->getValor());
+        static::assertEquals(1500, $maiores[2]->getValor());
+    }
 }
